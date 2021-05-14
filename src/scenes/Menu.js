b2fae024ef0f4b3d4c1b100b4e5fb76ca5ae4a96
ownagesauce <ejtung@ucsc.edu>
2021-05-13 22:47:46 -0700

    Added temporary menu UI

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,6 +3,43 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
     create() {
-        this.scene.start('playScene');  
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // font config
+        
+        let menuConfig = {
+
+            fontFamily: 'pixelfont',
+            fontSize: '36px',
+            color: '#FFFFFF',
+            stroke: '#000000',
+            strokeThickness: 3,
+            align: 'center'
+
+        }
+
+        // ui
+    
+        menuConfig.fontSize = '24px';
+        this.add.text(1080 / 2, 720 / 2 - 100, 'ARROW KEYS TO MOVE', menuConfig).setOrigin(0.5);
+        this.add.text(1080 / 2, 720 / 2 - 75, 'Z TO SLASH', menuConfig).setOrigin(0.5);
+        this.add.text(1080 / 2, 720 / 2 - 50, 'X TO SHOOT', menuConfig).setOrigin(0.5);
+        this.add.text(1080 / 2, 720 / 2 - 25, 'SHIFT TO TARGET LOCK', menuConfig).setOrigin(0.5);
+
+        menuConfig.fontSize = '36px';
+        this.add.text(1080 / 2, 720 / 2 + 50, 'PRESS SPACE TO CONTINUE', menuConfig).setOrigin(0.5);
+
     }
+
+    update() {
+
+        if (this.cursors.space.isDown) {
+
+            this.scene.start("playScene");  
+  
+        }
+
+    }
+
 }
